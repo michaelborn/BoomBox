583e4232b38f88acbe29d30870f2837ce422e1fa
Michael Born <mborn319@gmail.com>
2016-02-28 15:09:34 -0500

    Added required ID params, error responses.

--- api/routes.js
+++ api/routes.js
@@ -4,46 +4,95 @@ module.exports = function(app) {
   // Routes for getting, creating, updating, and deleting song tracks by ID.
   // ID is required for all except GET.
   app.get(api_version_str+'/track', function(req, res) {
+    if (typeof req.query.id === "undefined") {
+      //then return all tracks.
+    } else {
+      //only return track info for the track:ID
+    }
     //return tracks!
   });
-  app.post(api_version_str+'/track', function(req, res) {
-    //insert tracks!
+  app.post(api_version_str+'/track/:id', function(req, res) {
+    if (typeof req.params.id === "undefined") {
+      res.status(400).json({error: "You must specify an ID."});
+    } else {
+      //insert tracks!
+    }
   });
-  app.put(api_version_str+'/track', function(req, res) {
-    //update tracks!
+  app.put(api_version_str+'/track/:id', function(req, res) {
+    if (typeof req.params.id === "undefined") {
+      res.status(400).json({error: "You must specify an ID."});
+    } else {
+      //update track with trackID=req.
+    }
   });
-  app.delete(api_version_str+'/track', function(req, res) {
-    //delete tracks!
+  app.delete(api_version_str+'/track/:id', function(req, res) {
+    if (typeof req.params.id === "undefined") {
+      res.status(400).json({error: "You must specify an ID."});
+    } else {
+      //delete tracks!
+    }
   });
 
   // Routes for getting, creating, updating, and deleting song artists by ID.
   // ID is required for all except GET.
   app.get(api_version_str+'/artist', function(req, res) {
-    //return artists!
+    if (typeof req.query.id === "undefined") {
+      //then return all tracks.
+    } else {
+      //return artists!
+    }
   });
-  app.post(api_version_str+'/artist', function(req, res) {
-    //create artists!
+  app.post(api_version_str+'/artist/:id', function(req, res) {
+    if (typeof req.params.id === "undefined") {
+      res.status(400).json({error: "You must specify an ID."});
+    } else {
+      //create artists!
+    }
   });
-  app.put(api_version_str+'/artist', function(req, res) {
-    //create artists!
+  app.put(api_version_str+'/artist/:id', function(req, res) {
+    if (typeof req.params.id === "undefined") {
+      res.status(400).json({error: "You must specify an ID."});
+    } else {
+      //create artists!
+    }
   });
-  app.delete(api_version_str+'/artist', function(req, res) {
-    //delete artists!
+  app.delete(api_version_str+'/artist/:id', function(req, res) {
+    if (typeof req.params.id === "undefined") {
+      res.status(400).json({error: "You must specify an ID."});
+    } else {
+      //delete artists!
+    }
   });
 
   // Routes for getting, creating, updating, and deleting song albums by ID.
   // ID is required for all except GET.
   app.get(api_version_str+'/album', function(req, res) {
-    //return albums!
+    if (typeof req.query.id === "undefined") {
+      //then return all tracks.
+    } else {
+      //return albums!
+    }
   });
-  app.post(api_version_str+'/album', function(req, res) {
-    //create albums!
+  app.post(api_version_str+'/album/:id', function(req, res) {
+    if (typeof req.params.id === "undefined") {
+      res.status(400).json({error: "You must specify an ID."});
+    } else {
+      //create albums!
+    }
   });
-  app.put(api_version_str+'/album', function(req, res) {
-    //update albums!
+  app.put(api_version_str+'/album/:id', function(req, res) {
+    if (typeof req.params.id === "undefined") {
+      res.status(400).json({error: "You must specify an ID."});
+    } else {
+      //update albums!
+    }
   });
-  app.delete(api_version_str+'/album', function(req, res) {
-    //delete albums!
+  app.delete(api_version_str+'/album/:id', function(req, res) {
+    if (typeof req.params.id === "undefined") {
+      res.status(400).json({error: "You must specify an ID."});
+    } else {
+      //delete albums!
+    }
   });
   return app;
 };
