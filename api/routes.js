21991ba4cc9eaf771b38f5129eae712e8f60039c
Michael Born <mborn319@gmail.com>
2016-02-28 14:33:46 -0500

    Added versioning to routes.

--- api/routes.js
+++ api/routes.js
@@ -1,46 +1,48 @@
 module.exports = function(app) {
+  var api_version_str = "/api/v1";
+
   // Routes for getting, creating, updating, and deleting song tracks by ID.
   // ID is required for all except GET.
-  app.get('/api/track', function(req, res) {
+  app.get(api_version_str+'/track', function(req, res) {
     //return tracks!
   });
-  app.post('/api/track', function(req, res) {
+  app.post(api_version_str+'/track', function(req, res) {
     //insert tracks!
   });
-  app.put('/api/track', function(req, res) {
+  app.put(api_version_str+'/track', function(req, res) {
     //update tracks!
   });
-  app.delete('/api/track', function(req, res) {
+  app.delete(api_version_str+'/track', function(req, res) {
     //delete tracks!
   });
 
   // Routes for getting, creating, updating, and deleting song artists by ID.
   // ID is required for all except GET.
-  app.get('/api/artist', function(req, res) {
+  app.get(api_version_str+'/artist', function(req, res) {
     //return artists!
   });
-  app.post('/api/artist', function(req, res) {
+  app.post(api_version_str+'/artist', function(req, res) {
     //create artists!
   });
-  app.put('/api/artist', function(req, res) {
+  app.put(api_version_str+'/artist', function(req, res) {
     //create artists!
   });
-  app.delete('/api/artist', function(req, res) {
+  app.delete(api_version_str+'/artist', function(req, res) {
     //delete artists!
   });
 
   // Routes for getting, creating, updating, and deleting song albums by ID.
   // ID is required for all except GET.
-  app.get('/api/album', function(req, res) {
+  app.get(api_version_str+'/album', function(req, res) {
     //return albums!
   });
-  app.post('/api/album', function(req, res) {
+  app.post(api_version_str+'/album', function(req, res) {
     //create albums!
   });
-  app.put('/api/album', function(req, res) {
+  app.put(api_version_str+'/album', function(req, res) {
     //update albums!
   });
-  app.delete('/api/album', function(req, res) {
+  app.delete(api_version_str+'/album', function(req, res) {
     //delete albums!
   });
   return app;
