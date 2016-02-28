f7ed1f8da99f92cc2a2511fa7a3385b6141c0ca9
Michael Born <mborn319@gmail.com>
2016-02-28 14:20:58 -0500

    Added get, create, update, delete api routes for tracks, albums, and artists.

--- api/routes.js
+++ api/routes.js
@@ -1,8 +1,47 @@
 module.exports = function(app) {
-  // a simple route - return hello world when a url is accessed.
+  // Routes for getting, creating, updating, and deleting song tracks by ID.
+  // ID is required for all except GET.
   app.get('/api/track', function(req, res) {
     //return tracks!
   });
+  app.post('/api/track', function(req, res) {
+    //insert tracks!
+  });
+  app.put('/api/track', function(req, res) {
+    //update tracks!
+  });
+  app.delete('/api/track', function(req, res) {
+    //delete tracks!
+  });
 
+  // Routes for getting, creating, updating, and deleting song artists by ID.
+  // ID is required for all except GET.
+  app.get('/api/artist', function(req, res) {
+    //return artists!
+  });
+  app.post('/api/artist', function(req, res) {
+    //create artists!
+  });
+  app.put('/api/artist', function(req, res) {
+    //create artists!
+  });
+  app.delete('/api/artist', function(req, res) {
+    //delete artists!
+  });
+
+  // Routes for getting, creating, updating, and deleting song albums by ID.
+  // ID is required for all except GET.
+  app.get('/api/album', function(req, res) {
+    //return albums!
+  });
+  app.post('/api/album', function(req, res) {
+    //create albums!
+  });
+  app.put('/api/album', function(req, res) {
+    //update albums!
+  });
+  app.delete('/api/album', function(req, res) {
+    //delete albums!
+  });
   return app;
 };
