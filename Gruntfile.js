180cec64af8fd3b771390ee530d5cfaab29d006d
Michael Born <mborn319@gmail.com>
2016-03-01 17:28:29 -0500

    Added CSS linting and minification, JS uglification grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,27 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       build: {
-        src: 'app/css/app.css',
-        dest: '/app/css/app.min.css',
+        src: 'app/js/app.js',
+        dest: '/app/css/app.min.js',
       },
-    }
+    },
     concat: {
       dist: {
-        src: ['app/css/reset.css','app/css/grid.css','app/css/main.css'],
-        dest: 'app/css/app.css',
+        src: ['app/js/reset.js','app/js/grid.js','app/js/main.js'],
+        dest: 'app/js/app.js',
+      },
+    },
+    csslint: {
+      options: {
+        csslintrc: '.csslintrules',
+      },
+      src: ['app/css/reset.css','app/css/grid.css','app/css/main.css'],
+    },
+    cssmin: {
+      target: {
+        files: {
+          'app/css/app.css': ['app/css/reset.css','app/css/grid.css','app/css/main.css']
+        },
       },
     },
   });
@@ -19,7 +32,11 @@ module.exports = function(grunt) {
   // load grunt plugins
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Setup tasks to run when we execute `grunt build`
-  grunt.registerTask('build', ['concat','uglify']);
-};
+  grunt.registerTask('css', ['csslint','cssmin']);
+  grunt.registerTask('js', ['concat','jshint','uglify']);
+}
