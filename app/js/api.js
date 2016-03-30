0b1f7d301fcf40e5045832f24a19a85ff1fd84f6
Michael Born <mborn319@gmail.com>
2016-03-30 16:53:25 -0400

    Added API object, songs.get method.

--- app/js/api.js
+++ app/js/api.js
@@ -1,2 +1,17 @@
 // api.js - this file does all the data interaction with the BoomBox API
+var Api = function() {
+  this.songs = {};
+  this.albums = {};
+  this.artists = {};
+
+  this.songs.get = function(opts) {
+    var data = opts; 
+    var whenDone = function(dat) {
+      console.log("Ajax response:",dat);
+    };
+    lib.ajax("track",data,whenDone,"GET");
+  };
+
+};
+var api = new Api();
 
