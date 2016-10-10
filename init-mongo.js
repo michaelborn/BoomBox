e31a5a08561c6ff105db16e79353635124597a29
Michael Born <mborn319@gmail.com>
2016-10-10 15:01:11 -0400

    mongo init to 'boombox' db, works well

--- init-mongo.js
+++ init-mongo.js
@@ -4,7 +4,8 @@ var mongodb = require('mongodb'),
     MongoClient = mongodb.MongoClient,
     mongojs = require('mongojs');
 
-var db = mongojs("test")
+var dbUrl = "boombox",
+    collections = ["tracks","artists","albums"],
+    db = mongojs(dbUrl,collections);
+
 module.exports = db;
-
-
