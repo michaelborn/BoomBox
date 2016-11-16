38419451ae4dc1fadd36f66aca643606c472fda9
Michael Born <mborn319@gmail.com>
2016-11-15 21:39:55 -0500

    Got HTTPS workingfg

--- index.js
+++ index.js
@@ -5,13 +5,46 @@ var express = require("express"),
     path = require("path"),
     mu2Express = require("mu2express"),
     exec = require("child_process").exec,
-    db = require("./init-mongo.js"),
-    wss = require("./websockets.js");
+    https = require("https"),
+    fs = require("fs"),
+    db = require("./init-mongo.js");
 
-var sSettings = {
+var sOpts = {
   port: 8080,
-  ip: "0.0.0.0"
+  ip: "0.0.0.0",
+  key: fs.readFileSync(path.join(__dirname, 'certstuff', 'certs', 'server', 'privkey.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'certstuff', 'certs', 'server', 'fullchain.pem'))
 };
+
+//set up a templating engine
+app.engine("mustache", mu2Express.engine);
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "app/views"));
+
+// allow GET requests for static files in the /app folder.
+app.use("/app", express.static(__dirname + "/app"));
+
+// our main template
+app.get("/", function(req, res) {
+  res.render("main", {url:req.url});
+});
+
+// SSL (or TLS???) certificate
+
+// final server setup
+console.log("Server");
+server = https.createServer({ key: sOpts.key, cert: sOpts.cert },app);
+
+//start the server
+server.listen(sOpts.port, sOpts.ip, function() {
+  console.log("..... listening on port ", sOpts.port);
+});
+
+// setup the websocket connection with HTTPS
+var socketServer = require("ws").Server,
+    wss = new socketServer({ server: server });
+console.info("WebSocket");
+console.info("...... listening on port ", sOpts.port);
 
 // listen for a web socket connection from the user(s)
 wss.on("connection", function(ws) {
@@ -27,22 +60,3 @@ wss.on("connection", function(ws) {
   });
 });
 
-//set up a templating engine
-app.engine("mustache", mu2Express.engine);
-app.set("view engine", "mustache");
-app.set("views", path.join(__dirname, "app/views"));
-
-// allow GET requests for static files in the /app folder.
-app.use("/app", express.static(__dirname + "/app"));
-
-// our main template
-app.get("/", function(req, res) {
-  res.render("main", {url:req.url});
-});
-
-
-//start the server
-console.log("Server");
-app.listen(sSettings.port, sSettings.ip, function() {
-  console.log("..... listening on port ", sSettings.port);
-});
--- websockets.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-var socketServer = require("ws").Server,
-    wsSettings = { port: 8081 },
-    wss = new socketServer(wsSettings);
-
-console.info("WebSocket");
-console.info("...... listening on port ", wsSettings.port);
-
-module.exports = wss;
